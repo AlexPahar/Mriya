f38c1f2183e18461933d77f88fbc4a543a24c0a0
Enthelia <sharlo190599@gmail.com>
2020-10-12 23:12:19 +0700

    Fix tabs js

--- js/tabs.js
+++ js/tabs.js
@@ -1,6 +1,8 @@
 (function($) {
 	'use strict';
 
+	let Scrollbar = window.Scrollbar;
+
 	$(function() {
 		let menu_tab = $('.tabsmenu li'); 
 		if (!menu_tab.length)
@@ -20,9 +22,12 @@
 			return
 			
 		$('.tabsmenu-wrapper').find('.active-container').fadeOut(500, () => {
+			Scrollbar.destroy(document.querySelector('.active-container'))
 			container_tab.removeClass('active-container')
+
 			container_tab.eq(index).addClass('active-container')
 			container_tab.eq(index).css('display','flex').hide().fadeIn(500)
+			Scrollbar.init(document.querySelector('.active-container'))
 			container_tab.eq(index).css('display','flex').stop()
 		})
 	}
